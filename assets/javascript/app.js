62ca0ec4bdd055e63a5449ebda366cb453b201f1
Smack <Smack@Macintosh.local>
2017-11-30 11:27:44 -0500

    a lot of CSS, created splash page, added question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,12 @@
+
 $(document).ready(function() {
+
+
+// on start click fade from splash screen to main screen
+// trigger countdown timer as well
+$('.start_link').click(function() { 
+        $(this).parent().fadeOut(500);
 // countdown timer
-	var timeLeft = 10;
-	var elem = document.getElementById('timer');
-	var timerId = setInterval(countdown, 1000);
-	
 	function countdown() {
 	 	if (timeLeft === 0) {
 	    clearTimeout(timerId);
@@ -12,9 +15,23 @@ $(document).ready(function() {
 	    elem.innerHTML = timeLeft + ' seconds remaining';
 	    timeLeft--;
 	  }
+	
 	};
+	
+var timeLeft = 30;
+var elem = document.getElementById('timer');
+var timerId = setInterval(countdown, 1000);
 
 
+// when the submit button is clicked it will set variable timeleft to 0 
+// which will trigger the timeUp function
+	$('#submit').click(function(){
+		timeLeft = 0;
+	});
+
+ });
+
+// our 'score' variables
 var wrongCount = 0;
 var unansweredCount = 0;
 var correctCount = 0;
@@ -27,7 +44,8 @@ function timeUp(){
 		var Q2 = $('input:radio[name="q2"]:checked').val();
 		var Q3 = $('input:radio[name="q3"]:checked').val();
 		var Q4 = $('input:radio[name="q4"]:checked').val();
-		
+		var Q5 = $('input:radio[name="q5"]:checked').val();
+
 
 
 		// Determine the right/wrong/unanswered counts for each question 
@@ -74,22 +92,24 @@ function timeUp(){
 			wrongCount++;
 		};
 
-		$("#results").html('You answered ' + correctCount + ' of 4 correct');
+		if(Q5 == undefined){
+			unansweredCount++;
+		}
+		else if(Q5 == "Gungan"){
+			correctCount++;
+		}
+		else{
+			wrongCount++;
+		};
+
+// our results are concatenated and then displayed when timer is up
+		$("#results").html('You answered ' + correctCount + ' of 5 correct');
 		$("#unanswered").html('You did not answer ' + unansweredCount + ' question(s)');
 
 		console.log(correctCount);
 		console.log(wrongCount);
 		console.log(unansweredCount);
 	};
-
-	
-		
-		
-		
-
-	
-
-
 	
 
 });
